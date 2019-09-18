3676a5a320f4dbd1eecaa3ccd1ecd9ddae076b5c
sunny <sunnychoudhury007@gmail.com>
2019-09-18 08:27:08 +0530

    update assignments

--- array-methods/4-array.js
+++ array-methods/4-array.js
@@ -30,12 +30,27 @@ var data = [
 // 1 human year = 7 dog year
 
 // your code goes here
-
+sum = 0;
+function dogYear (){
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].type == 'dog') {
+      sum += data[i].age * 7;
+}
 // Solution is 105
 
 // Write the same function using
 // 1. filter - for filtering the cat or dog
 // 2. map - to multiply human year to dog year
 // 3. reduce - to accumulate total age.
+
+function dogYear() {
+  var typeDog = data.filter(element => element.type == 'dog');
+  var dogYear = typeDog.map(element => element.age * 7);
+  var sum = dogYear.reduce((x, y) => x + y);
+
+  return sum;
+}
+
+dogYear();
 
 // Solution 105
